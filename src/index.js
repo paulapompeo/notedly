57c0eb60795c1aa54d03e13eee3de2884450511a
Paula Pompeo <paula.pompeo@macaiapp.com>
2022-08-13 17:05:34 +0200

    adds express

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => res.send('hello baby!!!!!'));
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
