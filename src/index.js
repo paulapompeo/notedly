e540c6014d3463550c254fda90e2971ad334a17d
Paula Pompeo <paula.pompeo@macaiapp.com>
2022-08-14 08:36:46 +0200

    creates mutations

--- src/index.js
+++ src/index.js
@@ -1,8 +1,72 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
-const app = express();
+// Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('hello baby!!!!!'));
+const notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
 
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Mutation {
+    newNote(content: String!): Note!
+  }
+
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
+  type Query {
+    hello: String
+    notes: [Note!]!
+    note(id: ID!): Note
+  }
+`;
+
+// Provide resolver functions for our schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello baby!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+
+  Mutation: {
+    newNote: (parent, args) => {
+      const noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Paula Pompeo'
+      };
+
+      notes.push(noteValue);
+
+      return noteValue;
+    }
+  }
+};
+
+const app = express();
+
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/' });
+
+// app.get('/', (req, res) => res.send('hello baby!!!!!'));
+// app.listen(port, () => console.log(`Listening on port ${port}!`));
+
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL server running at http://localhost:${port}${server.graphqlPath}`
+  )
+);
