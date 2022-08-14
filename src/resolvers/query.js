fc171e21f40acac4302b62d93913d484b34d9997
Paula Pompeo <paula.pompeo@macaiapp.com>
2022-08-14 16:43:46 +0200

    creates user data functionalities

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -3,6 +3,15 @@ module.exports = {
     return await models.Note.find();
   },
   note: async (parent, args, { models }) => {
-    return models.Note.findById(args.id);
+    return await models.Note.findById(args.id);
+  },
+  user: async (parent, { username }, { models }) => {
+    return await models.User.findOne({ username });
+  },
+  users: async (parent, args, { models }) => {
+    return await models.User.find({});
+  },
+  me: async (parent, args, { models, user }) => {
+    return await models.User.findById(user.id);
   }
 };
