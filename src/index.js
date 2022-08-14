bc32abb738ac06e7dc672db0ecdefe01949c4dbb
Paula Pompeo <paula.pompeo@macaiapp.com>
2022-08-14 09:51:46 +0200

    refactor - isolate schema and resolvers

--- src/index.js
+++ src/index.js
@@ -1,55 +1,17 @@
 const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 require('dotenv').config();
-const models = require('./models');
 
+// local module imports
 const db = require('./db');
+const models = require('mongoose');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
 
 // Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
 // Store the DB_HOST value as a variable
 const DB_HOST = process.env.DB_HOST;
-
-// Construct a schema, using GraphQL schema language
-const typeDefs = gql`
-  type Mutation {
-    newNote(content: String!): Note!
-  }
-
-  type Note {
-    id: ID!
-    content: String!
-    author: String!
-  }
-
-  type Query {
-    hello: String
-    notes: [Note!]!
-    note(id: ID!): Note
-  }
-`;
-
-// Provide resolver functions for our schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Hello baby!',
-    notes: async () => {
-      return await models.Note.find();
-    },
-    note: async (parent, args) => {
-      return models.Note.findById(args.id);
-    }
-  },
-
-  Mutation: {
-    newNote: async (parent, args) => {
-      return await models.Note.create({
-        content: args.content,
-        author: 'Paula Pompeo'
-      });
-    }
-  }
-};
 
 const app = express();
 
@@ -57,13 +19,16 @@ const app = express();
 db.connect(DB_HOST);
 
 // Apollo Server setup
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: () => {
+    return models;
+  }
+});
 
 // Apply the Apollo GraphQL middleware and set the path to /api
 server.applyMiddleware({ app, path: '/' });
-
-// app.get('/', (req, res) => res.send('hello baby!!!!!'));
-// app.listen(port, () => console.log(`Listening on port ${port}!`));
 
 app.listen({ port }, () =>
   console.log(
